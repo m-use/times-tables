62ca065bddafade0b92076401469ace31c716a85
Lindsey Baker <lindseybakermedia@gmail.com>
2016-03-01 14:28:01 -0700

    initiates slider function and dynamic table

--- TimesTables/ViewController.swift
+++ TimesTables/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
     @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
     @IBAction func numberSlider(sender: AnyObject) {
         
-        print(sliderValue)
+        table.reloadData()
         
     }
 
@@ -21,6 +25,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        var sliderNumberValue = Int(sliderValue.value)
+        
+        cell.textLabel?.text = String(sliderNumberValue * (indexPath.row + 1))
+        
+        return cell
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
